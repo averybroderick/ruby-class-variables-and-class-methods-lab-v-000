6d2dd0c110ef8f21e4dac240dc0ed1414dc8254d
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-04 18:35:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,4 +27,17 @@ class Song
     @@genres
   end
 
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      if genre_hash[genre]
+        genre_hash[genre] += 1
+      else
+        genre_hash[genre] = 1
+      end
+    end
+  end 
+
+
+
 end
